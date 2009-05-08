6ea7ab7b517296869bd39fbc4e4440e22c36182e
eleonard <eleonard@vernier.com>
2009-05-07 16:57:17 -0800

    Mini GC USB product id = 7.
    [git-p4: depot-paths = "//depot/SDK/GoIO_SDK/": change = 29657]

--- src/GoIO_DLL/GoIO_DLL_interface.cpp
+++ src/GoIO_DLL/GoIO_DLL_interface.cpp
@@ -206,7 +206,7 @@ GOIO_DLL_INTERFACE_DECL gtype_int32 GoIO_GetDLLVersion(
 	gtype_uint16 *pMinorVersion) //[o]
 {
 	*pMajorVersion = 2;
-	*pMinorVersion = 25;
+	*pMinorVersion = 26;
 	return 0;
 }
 
